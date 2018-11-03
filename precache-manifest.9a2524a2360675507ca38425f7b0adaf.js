23a0443995f8bf416b79c2a0616ac3a55371d17a
Fanny Cheung <fanny@ynote.hk>
2018-11-03 15:00:27 +0100

    Updates

---
+++ precache-manifest.9a2524a2360675507ca38425f7b0adaf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9771ada3bb2d3fe2737",
+    "url": "/experiment-suspense/static/js/runtime~main.a9771ada.js"
+  },
+  {
+    "revision": "fd57329a74bd8d3eb926",
+    "url": "/experiment-suspense/static/js/main.fd57329a.chunk.js"
+  },
+  {
+    "revision": "2937c2fa85eb1ed88953",
+    "url": "/experiment-suspense/static/js/1.2937c2fa.chunk.js"
+  },
+  {
+    "revision": "77506ec2ae8ae6d01ff517e5b4e63291",
+    "url": "/experiment-suspense/index.html"
+  }
+];
